1ead42b1ba4cd0e2983028ac546d6babf67f6869
Bartosz Troń <kafka877@gmail.com>
2024-11-27 18:56:34 +0100

    add react-hot-toast && add icons lib

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
    title: "Droplo",
@@ -13,7 +14,20 @@ export default function RootLayout({
 }>) {
    return (
       <html lang="pl">
-         <body className={``}>{children}</body>
+         <Toaster
+            position="top-center"
+            gutter={12}
+            containerStyle={{ margin: "8px" }}
+            toastOptions={{
+               success: {
+                  duration: 3000,
+               },
+               error: {
+                  duration: 5000,
+               },
+            }}
+         />
+         <body className="max-w-[1208px] mx-auto rounded-md">{children}</body>
       </html>
    );
 }
